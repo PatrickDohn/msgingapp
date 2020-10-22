31b3fd71735104d55c189cdc0450e74e18a44b88
PatrickDohn <dohnpatrick1993@gmail.com>
2020-10-21 21:49:41 -0400

    installs pusher for frontend / updates app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Sidebar from './Sidebar/Sidebar';
 import Msg from './Msg/Msg'
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+    let pusher = new Pusher('0403401385d676f588cb', {
+      cluster: 'us2'
+    });
+
+    let channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
+
   return (
     <div className="app">
       <div className='app_body'>
